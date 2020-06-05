1af8949f5e992714310a2c90a54625bc47cbbee9
LightDesk <piyushsakhare713@gmail.com>
2020-06-05 21:19:42 +0530

    Second mobile optimization ( Header )

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return(
          <div >
              <div>
-             <header className=" bg-white shadow-lg fixed w-full flex  lg:mx-4 rounded text-xl justify-between lg:px-3 items-center">
+             <header className=" bg-white shadow-lg fixed w-full flex rounded text-xl justify-between lg:px-3 items-center">
                  <h1 className=" lg:px-4 py-3 font-extrabold text-purple-700" >&lt;Piyush /&gt;</h1>
                  <div className=" lg:pr-4  text-purple-700">
                     <Link className="lg:mx-3 rounded-lg border-2 p-2  border-purple-700 text-lg" to="/project" >Projects</Link>
@@ -19,7 +19,7 @@ function Header() {
 
                 </div>
                 <div className="lg:bg-purple-700 rounded lg:mr-32 ">
-                <img className="max-w-xs rounded-full inline z-10 lg:mt-16 lg:ml-12 shadow-2xl lg:m-16 m-12" src={profile} alt="profile"></img>
+                <img className="max-w-xs rounded-full inline z-10 lg:mt-16 lg:ml-12 shadow-2xl lg:m-16 my-12" src={profile} alt="profile"></img>
                 </div>
                 <div className="col-span-2 lg:mt-24 text-black lg:ml-32">
                     <h2 className="lg:text-5xl text-4xl font-thin" >Hi, I am  <br/> <strong className="font-bold">Piyush Sakhare</strong></h2>
